7cbfb4db28d8dbe91e948d06cd2f78cd6ab289b5
Peter Theill <peter@theill.com>
2009-09-08 15:59:06 +0200

    improved mail parsing and preparing for future types of mails

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -13,7 +13,7 @@ class Inbox
         parse(msg.first.attr["RFC822"])
         imap.store(message_id, "+FLAGS", [:Deleted])
       rescue Exception => e
-        puts "upsi #{e.message}"
+        puts "failed to process message #{e.message}"
         # Rails.logger.error "[" + Time.now.to_s + "] " + e.message
       end
     end
@@ -27,25 +27,38 @@ class Inbox
     mail = TMail::Mail.parse(msg)
     puts "Got mail #{mail.subject} from #{mail.from} #{mail.has_attachments? ? 'with' : 'without'} attachments"
     
-    user = User.find_by_email(mail.from)
-    if !user
-      puts "User with email #{mail.from} not found"
-      user = ::User.create(:email => mail.from, :password => '123456', :password_confirmation => '123456', :company => 'Anonymous Inc.')
-      if user.save
-        ::SignupMailer.deliver_created(user, mail.subject)
-      end
-    end
+    # lookup or create user
+    user = User.find_by_email(mail.from) || setup_user(mail.subject)
     
+    parsed_attributes = recognize_and_parse_mail(mail)
+    
+    posting = user.postings.create!(:note => parsed_attributes[:description], :account_id => 1, :amount => parsed_attributes[:amount])
+    associate_attachments(posting, mail) if mail.has_attachments?
+  end
+  
+  def setup_user(mail)
+    puts "User with email #{mail.from} not found so will be created"
+    user = ::User.new(:email => mail.from, :password => '123456', :password_confirmation => '123456', :company => 'My Company')
+    ::SignupMailer.deliver_created(user, mail.subject) if user.save
+    user
+  end
+  
+  def associate_attachments(posting, mail)
+    mail.attachments.each do |attachment|
+      posting.update_attribute(:attachment, attachment)
+    end
+  end
+  
+  def recognize_and_parse_mail(mail)
+  	# support@getharvest.com
+  	
     # do Harvest amount parsing
     parsed_amount = (mail.body || "").scan(/amount:\W?(\$)?([\d|\.]*)\W?\(?(DKK|USD|NOK|SEK|EUR)?\)?/i).flatten
     amount = (parsed_amount.length > 1) ? parsed_amount[1] : "0.0"
     
-    posting = user.postings.create!(:note => mail.subject, :account_id => 1, :amount => amount)
-    if mail.has_attachments?
-      mail.attachments.each do |attachment|
-        posting.update_attribute(:attachment, attachment)
-      end
-    end
+    description = mail.subject.gsub(/^Fwd: /, '')
+  	
+  	{ :amount => amount, :currency => "USD", :account_type => 'sale', :description => description }
   end
   
 end
