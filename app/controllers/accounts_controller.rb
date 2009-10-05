604446291f59ecd6b8b17766cf564a5427a847e2
Peter Theill <peter@theill.com>
2009-10-05 15:30:28 +0200

    added initial ability to edit/destroy accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,20 +7,34 @@ class AccountsController < ApplicationController
     @account = current_user.accounts.new
     
   end
+
+  # GET /accounts/1/edit
+  def edit
+    @account = current_user.accounts.find(params[:id])
+  end
   
   # POST /accounts
   def create
     @account = current_user.accounts.new(params[:account])
-
+    
     respond_to do |format|
       if @account.save
-        flash[:notice] = 'Account was successfully created.'
         format.html { redirect_to(accounts_url) }
         format.xml  { render :xml => @account, :status => :created, :location => @account }
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @account.errors, :status => :unprocessable_entity }
       end
+    end
+  end
+
+  # DELETE /accounts/1
+  def destroy
+    @account = current_user.accounts.find(params[:id])
+    @account.destroy
+    
+    respond_to do |format|
+      format.js
     end
   end
   
