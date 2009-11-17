e420ca89818f2fbf8079b566e5f49c50608b3a20
Peter Theill <peter@theill.com>
2009-11-17 09:19:05 +0100

    added start of spec for importings

---
+++ spec/app/models/posting_import_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe "An importing model" do
+  it "should recognize headers if available in first line" do
+    
+  end
+  
+  it "should recognize column separators" do
+    
+  end
+  
+  it "should import all entries from file" do
+    
+  end
+  
+  it "should filter existing transactions from file" do
+    
+  end
+  
+end
