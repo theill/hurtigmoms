2bf9ec5081aeb6f1f2ed23690fb287e8991c9e79
Peter Theill <peter@theill.com>
2010-01-29 15:29:39 +0100

    ignore ssl certificate when getting attachments from amazon

--- app/helpers/annexes_helper.rb
+++ app/helpers/annexes_helper.rb
@@ -1,16 +1,28 @@
+require 'net/https'
+require 'uri'
+
 module AnnexesHelper
   def format_as_plain_text(annex)
-    mail = TMail::Mail.parse(annex.authenticated_url)
-    # mail = TMail::Mail.parse(File.read("#{RAILS_ROOT}/test/fixtures/mails/harvest.txt"))
+    # msg = File.read("#{RAILS_ROOT}/test/fixtures/mails/harvest.txt")
+    msg = open(annex.authenticated_url).string
+    mail = TMail::Mail.parse(msg)
     
-    unless mail || mail.body.empty?
-      "<div id=\"mail-display\"><fieldset><label>Subject:</label> #{mail.subject}</fieldset>
+    if mail
+      "<div id=\"mail-display\">
+<div class=\"headers\">
+<fieldset><label>Subject:</label> #{mail.subject}</fieldset>
 <fieldset><label>From:</label> #{mail.from.to_s}</fieldset>
 <fieldset><label>Date:</label> #{mail.date}</fieldset>
+</div>
 
 <pre>#{mail.body}</pre></div>"
     else
-      "<div id=\"mail-display\">This mail could not be parsed.</div>"
+      "<div id=\"mail-warning-display\"><h3>Det var ikke muligt at vise denne besked</h3>
+      <p>
+        Vi understøtter endnu ikke formattet på denne besked. Du bliver nødt 
+        til at hente beskeden ned på din egen maskine for at se det.
+      </p>
+      </div>"
     end
   end
 end
