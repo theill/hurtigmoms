e0b052bbfd9429e467a69eaaf2f7cdde816c8ebb
Peter Theill <peter@theill.com>
2010-02-02 20:20:53 +0100

    using .length instead of .count

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,7 +2,7 @@ class AboutController < ApplicationController
   def index
     render :layout => 'frontpage' and return unless signed_in?
     
-    @action_required = current_user.active_fiscal_year.transactions.incomplete.count > 0
+    @action_required = current_user.active_fiscal_year.transactions.incomplete.length > 0
     @latest_import = current_user.posting_imports.find(:first, :order => 'created_at DESC')
   end
   
