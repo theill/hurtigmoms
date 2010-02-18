39c51d0f9b9512811653f28f221ce4570f86d140
Peter Theill <peter@theill.com>
2010-02-18 12:47:57 +0100

    removing hoptoad

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,3 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = '86b212d8d736073585ce32579bbffbe4'
-end
+# HoptoadNotifier.configure do |config|
+#   config.api_key = '86b212d8d736073585ce32579bbffbe4'
+# end
