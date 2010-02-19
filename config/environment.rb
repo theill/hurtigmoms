81926922ff0cfd454e08ccad162259a2a2f477ba
Peter Theill <peter@theill.com>
2010-02-19 10:58:03 +0100

    updating gems

--- config/environment.rb
+++ config/environment.rb
@@ -23,9 +23,9 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-paperclip', :version => '>=2.3.1', :lib => 'paperclip', :source => 'http://gems.github.com'
   config.gem 'jigfox-action_mailer_tls', :version => '>=1.1.3', :lib => 'smtp_tls.rb', :source => 'http://gems.github.com'
   config.gem 'fastercsv'
-  config.gem 'will_paginate', :version => '>=2.3.11', :source => 'http://gemcutter.org'
+  config.gem 'will_paginate', :version => '>=2.3.12', :source => 'http://gemcutter.org'
   config.gem 'exceptional', :version => '>=2.0.10'
-  config.gem 'rubyzip', :lib => 'zip/zip'
+  config.gem 'rubyzip', :version => '>=0.9.4', :lib => 'zip/zip'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
