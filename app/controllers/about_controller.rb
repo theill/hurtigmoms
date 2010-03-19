f9c707a5770c550ba0f9e075e40661134b2364af
Peter Theill <peter@theill.com>
2010-03-19 19:27:04 +0100

    optimized dashboard to show action required only once

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -11,13 +11,14 @@ class AboutController < ApplicationController
     @incomplete_transactions = current_user.active_fiscal_year.transactions.incomplete
     @transactions_without_related_transactions = current_user.active_fiscal_year.transactions.without_related_transactions
     
-    @action_required = @transactions_with_wrong_fiscal_year.any? || @incomplete_transactions.any? || @transactions_without_related_transactions.any?
+    latest_import = current_user.posting_imports.first(:order => 'created_at DESC')
+    @late_import = latest_import && (1.month.ago > latest_import.created_at)
+    
+    @action_required = @transactions_with_wrong_fiscal_year.any? || @incomplete_transactions.any? || @transactions_without_related_transactions.any? || @late_import
     
     if @transactions_with_wrong_fiscal_year.any?
       @other_fiscal_years = current_user.fiscal_years.delete_if { |fy| fy == current_user.active_fiscal_year }
     end
-    
-    @latest_import = current_user.posting_imports.find(:first, :order => 'created_at DESC')
   end
   
   def help
